8d38a859b6adbfc78034e5dabacc9d0ed8108b42
Adam-Young <awy6448@gmail.com>
2020-04-17 22:09:43 -0400

    created useEffect to pull character data from the rick and morty api. Created a component to render each char from the data set put into state with my axios call. Use reactstrap to create a card style to display data.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+import {CardImg, Card, CardBody, CardTitle, CardSubtitle, CardText, Col} from 'reactstrap'
+
+
+
+
+const Character = ({data}) => {
+
+return(
+    <Col>
+    <Card>
+        <CardImg src={data.image} alt="Card image cap" />
+        <CardBody>
+        <CardTitle>Name: {data.name}</CardTitle>
+        <CardSubtitle>Current location: {data.location.name}</CardSubtitle>
+        <CardSubtitle>Origin: {data.origin.name}</CardSubtitle>
+        <CardText>Species: {data.species}</CardText>
+        <CardText>Gender: {data.gender}</CardText>
+        <CardText>Current status: {data.status}</CardText>
+        </CardBody>
+    </Card>
+    </Col>
+)}
+
+export default Character;
